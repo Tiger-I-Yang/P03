0d700cbbe82ccbf785033186cac9d853bf564da4
Tiger Yang <tigeryangca@outlook.com>
2024-03-24 16:38:38 -0700

    style evperimentation

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 
 (async () => {
 
+    const updatePerspective = () => {
+        document.body.style.perspectiveOrigin = `${window.innerWidth/2 + window.scrollX}px ${window.innerHeight/2 + window.scrollY}px`
+    }
+
+    updatePerspective()
     document.addEventListener(
         'scroll', 
-        () => {
-			document.body.style.perspectiveOrigin = `${window.innerWidth/2 + window.scrollX}px ${window.innerHeight/2 + window.scrollY}px`
-        },
+        updatePerspective,
         {capture: false, passive: true}
+    )
+    window.addEventListener(
+        'resize', 
+        updatePerspective,
+        {capture: true, passive: true}
     )
 
 	
