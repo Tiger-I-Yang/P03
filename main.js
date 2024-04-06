3293841f9b332c30bdebc75079d62dc03fc2c979
Tiger Yang <tigeryangca@outlook.com>
2024-04-06 16:34:14 -0700

    process analysis 2 complete

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
 
 (async () => {
 
-    const scene = document.getElementById('scene')
-
-    const updatePerspective = () => {
-        scene.style.perspectiveOrigin = `50% calc(50% + ${window.scrollY}px)`
-    }
-
-    updatePerspective()
-    document.addEventListener(
-        'scroll', 
-        updatePerspective,
-        {capture: false, passive: true}
-    )
-
 })()
